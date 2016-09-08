56e358e84009d6460dfb294cd8b83dd70f25ab3c
jackytianhappy <1142899950@qq.com>
2016-09-08 21:50:06 +0800

    添加高的map

--- TestWithGaodeMap/TestWithGaodeMap/TestWithGaodeMap/ViewController.m
+++ TestWithGaodeMap/TestWithGaodeMap/TestWithGaodeMap/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
+@interface ViewController ()<MKMapViewDelegate>
+
+//@property (nonatomic, strong) MKMapView *mapView;
 
 @end
 
@@ -17,6 +20,12 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    ///初始化地图
+    MKMapView *_mapView = [[MKMapView alloc] initWithFrame:self.view.bounds];
+    
+    ///把地图添加至view
+    [self.view addSubview:_mapView];
 }
 
 - (void)didReceiveMemoryWarning {
