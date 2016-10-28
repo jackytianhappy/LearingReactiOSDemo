49595f2f838d8feab0ae22c7e18031b4a4fc580e
jackytianhappy <1142899950@qq.com>
2016-10-28 10:58:44 +0800

    添加attribute绘制文本的例子

---
+++ CoreTextDemo/CoreTextDemo/CoreTextDemo/CoreTextView.h
@@ -0,0 +1,13 @@
+//
+//  CoreTextView.h
+//  CoreTextDemo
+//
+//  Created by Jacky on 16/10/28.
+//  Copyright © 2016年 jacky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CoreTextView : UIView
+
+@end
--- CoreTextDemo/CoreTextDemo/CoreTextDemo/ViewController.m
+++ CoreTextDemo/CoreTextDemo/CoreTextDemo/ViewController.m
@@ -8,6 +8,7 @@
 
 #import "ViewController.h"
 #import <CoreText/CoreText.h>
+#import "CoreTextView.h"
 
 
 @interface ViewController ()
@@ -20,38 +21,30 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    _testLbl = [[UILabel alloc]initWithFrame:CGRectMake(100, 100, 300, 40)];
-    [self.view addSubview:self.testLbl];
-    
-    NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc]initWithString:@"这里是一条测试数据,希望这条数据尽可能的长吧，这样我们能够进行充分的展示"];
-    //设置字体大小
-    [attrString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:30] range:NSMakeRange(0, 3)];
-    //设置字体颜色
-    [attrString addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:NSMakeRange(4, 1)];
-    //设置字体背景颜色
-    [attrString addAttribute:NSBackgroundColorAttributeName value:[UIColor orangeColor] range:NSMakeRange(5, 1)];
-    //在第一个字后面插入图片
-    NSString *imageName = @"test.png";
-    NSFileWrapper *imageFileWrapper = [[NSFileWrapper alloc] initRegularFileWithContents:[[NSImage imageNamed:imageName] TIFFRepresentation]];
-    imageFileWrapper.filename = imageName;
-    imageFileWrapper.preferredFilename = imageName;
-    
-    NSTextAttachment *imageAttachment = [[[NSTextAttachment alloc] initWithFileWrapper:imageFileWrapper] autorelease];
-    NSAttributedString *imageAttributedString = [NSAttributedString attributedStringWithAttachment:imageAttachment];
-    [attributedString insertAttributedString:imageAttributedString atIndex:1];
-    
-    
-    self.testLbl.attributedText = attrString;
-    
-//    NSString *name = @"12";
+//    _testLbl = [[UILabel alloc]initWithFrame:CGRectMake(100, 100, 300, 40)];
+//    [self.view addSubview:self.testLbl];
 //    
-//    @autoreleasepool {
-//        NSString *tmp  = @"1234";
-//        name = [tmp copy];
-//        NSLog(@"%@",tmp);
-//    }
-//    
-//    NSLog(@"%@",@"234");
+//    NSMutableAttributedString *attrString = [[NSMutableAttributedString alloc]initWithString:@"这里是一条测试数据,希望这条数据尽可能的长吧，这样我们能够进行充分的展示"];
+//    //设置字体大小
+//    [attrString addAttribute:NSFontAttributeName value:[UIFont systemFontOfSize:30] range:NSMakeRange(0, 3)];
+//    //设置字体颜色
+//    [attrString addAttribute:NSForegroundColorAttributeName value:[UIColor redColor] range:NSMakeRange(4, 1)];
+//    //设置字体背景颜色
+//    [attrString addAttribute:NSBackgroundColorAttributeName value:[UIColor orangeColor] range:NSMakeRange(5, 1)];
+//    //在第一个字后面插入图片
+//    NSString *imageName = @"test.png";
+//   
+//    self.testLbl.attributedText = attrString;
+   //＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊＊华丽的分割线
+    
+    
+//    CoreTextView *view = [[CoreTextView alloc]initWithFrame:CGRectMake(0, 0, 300, 300)];
+//    view.backgroundColor = [UIColor whiteColor];
+//    [self.view addSubview:view];
+    
+    
+    
+    
     
 }
 
