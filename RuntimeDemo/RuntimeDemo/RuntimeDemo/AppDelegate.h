097ee3674e56d24d50f1620f0eae7361c6cd01f2
jackytianhappy <1142899950@qq.com>
2016-12-06 16:15:45 +0800

    runtime文档学习笔记

---
+++ RuntimeDemo/RuntimeDemo/RuntimeDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RuntimeDemo
+//
+//  Created by Jacky on 16/12/6.
+//  Copyright © 2016年 jacky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RuntimeDemo/RuntimeDemo/RuntimeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RuntimeDemo
+//
+//  Created by Jacky on 16/12/6.
+//  Copyright © 2016年 jacky. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
